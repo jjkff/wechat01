47fadd987295112f53c0bcb5f530adf0edc950c5
jkf <123456>
2017-05-28 09:51:09 +0800

    删除

--- src/test/java/aaaa.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Sierac on 2017/5/28.
- */
-public class aaaa {
-}
--- src/test/java/com/dongliushui/saxparsetest/defg.java
+++
@@ -1,9 +0,0 @@
-package com.dongliushui.saxparsetest;
-
-/**
- * Created by Sierac on 2017/5/28.
- */
-public class defg {
-
-
-}
